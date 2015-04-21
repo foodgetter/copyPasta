3745ef7dd3bf9427897e840cab3ed7b92f0b811e
foodgetter <lolz657@gmail.com>
2015-04-21 20:37:23 +0500

    Save Script page button formatting added

--- bgscript.js
+++ bgscript.js
@@ -1,10 +1,12 @@
+savewindowstart_h = 
+
 chrome.commands.onCommand.addListener(function(command) {
 	if(command == "start_copy"){
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   			chrome.tabs.sendMessage(tabs[0].id, {type: "startcopy"});
   			});
   		} else if(command == "start_save"){
-  			window.open("save_page.html", "copyPasta", "height=620,width=560");
+  			window.open("save_page.html", "copyPasta", "height=630,width=565");
   		}
 });
 
